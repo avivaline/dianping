e172665c5331ff95fddde449ce27186ed4b06548
aviva <avivaline@163.com>
2020-11-11 20:39:55 +0800

    configuation of sale table

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,6 +1,6 @@
 
 
-CREATE TABLE `dianping`.`Untitled`  (
+CREATE TABLE `dianping`.`user`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
   `created_at` datetime(0) NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` datetime(0) NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -10,4 +10,14 @@ CREATE TABLE `dianping`.`Untitled`  (
   `gender` int(0) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `telphone_unique_index`(`telphone`) USING BTREE
+);
+
+CREATE TABLE `dianping`.`sale`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL DEFAULT "",
+  `created_at` datetime(0) NOT NULL DEFAULT "0000-00-00 00:00:00",
+  `updated_at` datetime(0) NOT NULL DEFAULT "0000-00-00 00:00:00",
+  `remark_score` decimal(2, 1) NOT NULL DEFAULT 0,
+  `disable_flag` int(0) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
 );
